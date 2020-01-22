365507e6eef5aacec40e72ffda3f3f325dfcc442
shaoliang.pang <shaoliang.pang@100credit.com>
2020-01-23 00:11:43 +0800

    :sparkles: 登录功能提交

---
+++ src/components/Login.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img alt src="../assets/logo.png" />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        :model="loginForm"
+        :rules="loginFormRules"
+        class="login_form"
+        label-width="0px"
+        ref="loginFormRef"
+      >
+        <el-form-item prop="username">
+          <el-input prefix-icon="el-icon-user" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock" type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button @click="login" type="primary">登录</el-button>
+          <el-button @click="resetForm('loginFormRef')" type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: 'fff',
+        password: 'fff'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) {
+          return
+        }
+        const { data: res } = await this.$http.get('/user.json', this.loginForm)
+        if (res.code !== 200) return this.$message.error('登录失败')
+        console.log(res)
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.avatar_box {
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  position: absolute;
+  box-shadow: 0 0 10px #ddd;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/global.css'
 import router from './router'
 import store from './store'
-
+// 配置请求的配置
+// axios.defaults.baseURL = 'http://127.0.0.1:8080/assets/json/user.json'
+Vue.prototype.$http = axios
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
